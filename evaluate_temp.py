e72523e90cb4ed413e9b59963d9561c4d9996cff
rsw home pc <rw2967@columbia.edu>
2024-09-02 01:10:05 -0400

    edited evaluate_temp so that it accepts arguments.

--- evaluate_temp.py
+++ evaluate_temp.py
@@ -15,6 +15,7 @@ import copy
 from datetime import datetime
 from typing import Optional, List
 import sys
+import argparse
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from CogModelingRNNsTutorial.CogModelingRNNsTutorial import bandits, disrnn, hybrnn, plotting, rat_data, rnn_utils
 from train_disrnn import load_data, to_onehot, zs_to_onehot, dataset_size, create_train_test_datasets, preprocess_data
@@ -22,23 +23,8 @@ from train_disrnn import load_data, to_onehot, zs_to_onehot, dataset_size, creat
 warnings.filterwarnings("ignore")
 
 # Model-related functions
-def make_disrnn():
-    model = disrnn.HkDisRNN(
-        ### TODO: changed to argument for obs_size
-        obs_size=6,
-        target_size=2,
-        latent_size=16,
-        update_mlp_shape=(2, 2),
-        choice_mlp_shape=(2, 2),
-        eval_mode=0.0,
-        beta_scale=3,
-        activation=jax.nn.relu
-    )
-    return model
-
-def compute_log_likelihood_and_accuracy(dataset, model_fun, params):
-    xs, actual_choices = next(dataset)
-    n_trials_per_session, n_sessions = actual_choices.shape[:2]
+def compute_log_likelihood_and_accuracy(xs, ys, model_fun, params):
+    n_trials_per_session, n_sessions = ys.shape[:2]
     model_outputs, _ = rnn_utils.eval_model(model_fun, params, xs)
     predicted_log_choice_probabilities = np.array(jax.nn.log_softmax(model_outputs[:, :, :2]))
 
@@ -48,7 +34,7 @@ def compute_log_likelihood_and_accuracy(dataset, model_fun, params):
 
     for sess_i in range(n_sessions):
         for trial_i in range(n_trials_per_session):
-            actual_choice = int(actual_choices[trial_i, sess_i])
+            actual_choice = int(ys[trial_i, sess_i])
             if (actual_choice >= 0) and (0 <= actual_choice < predicted_log_choice_probabilities.shape[2]):
                 log_likelihood += predicted_log_choice_probabilities[trial_i, sess_i, actual_choice]
                 predicted_choice = np.argmax(predicted_log_choice_probabilities[trial_i, sess_i])
@@ -307,7 +293,7 @@ def plot_update_rules(params, make_network):
 
   # Loop over latents. Plot update rules
   for latent_i in latent_order:
-    # If this latent's bottleneck is open
+    # If this latent's bottleneck is o 
     if latent_sigmas[latent_i] < 0.5:
       # Which of its input bottlenecks are open?
       update_mlp_inputs = np.argwhere(update_sigmas[latent_i] < 0.9)
@@ -407,20 +393,20 @@ def plot_training_results(df_train, dataset_train):
     plt.close()
 
 def create_dataframe_for_plotting(predictions, actions, bitResponseAIsCorr, P_A, xs):
-    _c0 = actions == 0
-    _c1 = actions == 1
-    _cn = ~np.isin(actions, [0, 1])
-    _c = np.concatenate([_c0, _c1, _cn], axis=2)
-    _nan = np.empty([*predictions.shape[:2], 1])
-    _nan.fill(np.nan)
-    _pred_expand = np.concatenate([predictions, _nan], axis=2)
-    _pred_acc = _pred_expand[_c]
-    _pred_acc = _pred_acc.reshape(actions.shape)
+    action_0 = actions == 0
+    action_1 = actions == 1
+    action_others = ~np.isin(actions, [0, 1])
+    action_oh = np.concatenate([action_0, action_1, action_others], axis=2)
+    nan = np.empty([*predictions.shape[:2], 1])
+    nan.fill(np.nan)
+    predictions_expanded = np.concatenate([predictions, nan], axis=2)
+    prediction_prob = predictions_expanded[action_oh] # Prediction_prob shape (20000, )
+    prediction_prob = prediction_prob.reshape(actions.shape)
     
     df = pd.DataFrame({
         'act': actions[:, :, 0].ravel(),  # human-chosen action
         'pred': np.exp(predictions[:, :, 1].ravel()),  # model-predicted action
-        'pred_acc': np.exp(_pred_acc.ravel()),  # model-predicted probability of the human-chosen action
+        'pred_acc': np.exp(prediction_prob.ravel()),  # model-predicted probability of the human-chosen action
         'bitResponseAIsCorr': bitResponseAIsCorr,  # Correct optimal choices of the experiment
         'P_A': P_A,  # Human probability of choosing action 0
         # TODO: Change how dataframe is organized
@@ -428,16 +414,6 @@ def create_dataframe_for_plotting(predictions, actions, bitResponseAIsCorr, P_A,
         'rwd': np.vstack((np.zeros((1, xs.shape[1])), xs[1:, :, 0])).ravel()
         # np.vstack((np.zeros((1, xs.shape[1])), xs[1:, :, 0])).ravel()  # Reward for human-chosen action
     })
-    '''
-    df = pd.DataFrame({
-        'act': actions[:, :, 0].ravel(),  # human-chosen action
-        'pred': np.exp(predictions[:, :, 1].ravel()),  # model-predicted action
-        'pred_acc': np.exp(_pred_acc.ravel()),  # model-predicted probability of the human-chosen action
-        'bitResponseAIsCorr': bitResponseAIsCorr[:, :, 0].ravel(),  # Correct optimal choices of the experiment
-        'P_A': P_A[:, :, 0].ravel(),  # Human probability of choosing action 0
-        'rwd': np.vstack((np.zeros((1, xs.shape[1])), xs[1:, :, 0])).ravel()  # Reward for human-chosen action
-    })
-'''
 
     # Compute the optimal choice and model-estimated probability
     # df['optimal_choice_manual'] = np.where(df['rwd'] == 1, df['act'], 1 - df['act'])
@@ -446,14 +422,15 @@ def create_dataframe_for_plotting(predictions, actions, bitResponseAIsCorr, P_A,
     # rwd_manual
     df['rwd_manual'] = np.where(df['act'] == df['bitResponseAIsCorr'], 1, 0)
 
-    log_probs = np.log(df['human_chosen_action_prob'])
-    normalized_likelihood_upper_bound = np.exp(np.mean(log_probs))
-    print("Model Normalized likelihood Upperbound", normalized_likelihood_upper_bound)
-
     return df
 
-def plot_bottlenecks(params, sort_latents=True, obs_names=None):
+def plot_bottlenecks(params, save_folder_name, sort_latents=True, obs_names=None):
     """Plot the bottleneck sigmas from an hk.CompartmentalizedRNN."""
+    plot_dir = 'plots/bottlenecks'
+    save_dir = os.path.join(plot_dir, save_folder_name)
+    os.makedirs(os.path.dirname(save_dir), exist_ok=True)  # Ensure the full directory path exists
+    save_path = f"{save_dir}.png"  # Save file directly to the full path with the given name
+
     params_disrnn = params['hk_dis_rnn']
     latent_dim = params_disrnn['latent_sigmas_unsquashed'].shape[0]
     obs_dim = params_disrnn['update_mlp_sigmas_unsquashed'].shape[0] - latent_dim
@@ -466,11 +443,11 @@ def plot_bottlenecks(params, sort_latents=True, obs_names=None):
         else: 
             obs_names = np.arange(1, obs_dim+1)
 
-    latent_sigmas = 2 * jax.nn.sigmoid(
+    latent_sigmas = 60 * jax.nn.sigmoid(
         jnp.array(params_disrnn['latent_sigmas_unsquashed'])
     )
 
-    update_sigmas = 2 * jax.nn.sigmoid(
+    update_sigmas = 60 * jax.nn.sigmoid(
         np.transpose(
             params_disrnn['update_mlp_sigmas_unsquashed']
         )
@@ -513,56 +490,76 @@ def plot_bottlenecks(params, sort_latents=True, obs_names=None):
     plt.title('Update MLP Bottlenecks')
 
     plt.tight_layout()
-    plt.savefig("plot_bottlenecks.png")
-
+    plt.savefig(save_path)  # Save the figure to the specified path
 
     return fig
 
-def main():
-    # Directories for saving files
-    checkpoint_dir = './checkpoints'
-    plot_dir = './plots'
-    loss_dir = './losses'
+def main(seed, saved_checkpoint_pth):
+    np.random.seed(seed)
 
     dataset_type = 'RealWorldKimmelfMRIDataset'  
     dataset_path = "dataset/tensor_for_dRNN_desc-syn_nSubs-2000_nSessions-1_nBlocks-1_nTrialsPerBlock-100_b-0.3_NaN_30_0.93_0.45_NaN_NaN_withOptimalChoice_20240718_fast.mat"
     dataset_train, dataset_test, bitResponseAIsCorr_train, bitResponseAIsCorr_test, P_A_train, P_A_test = preprocess_data(dataset_type, dataset_path, 0.1)
 
     # Load the checkpoint parameters
-    checkpoint_name = 'checkpoints/ls_16_umlp_[2, 2]_cmlp_[2, 2]_beta_1e-05_penalty_1e-10_20240901_001206/disrnn_params_ls_16_umlp_[2, 2]_cmlp_[2, 2]_penalty_1e-10_beta_1e-05_lr_1e-3.pkl'
-    with open(checkpoint_name, 'rb') as file:
+    with open(saved_checkpoint_pth, 'rb') as file:
         checkpoint = pickle.load(file)
     args_dict = checkpoint['args_dict']
     disrnn_params = checkpoint['disrnn_params']
-    print(f'Loaded disrnn_params from {checkpoint_name}')
+    print(f'Loaded disrnn_params from {saved_checkpoint_pth}')
+
+    def make_disrnn():
+      model = disrnn.HkDisRNN(
+          obs_size=6,
+          target_size=2,
+          latent_size=args_dict['latent_size'],
+          update_mlp_shape=args_dict['update_mlp_shape'],
+          choice_mlp_shape=args_dict['choice_mlp_shape'],
+          eval_mode=0.0,
+          beta_scale=args_dict['beta_scale'],
+          activation=jax.nn.relu
+      )
+      return model
+
+    xs_train, ys_train = next(dataset_train)
+    xs_test, ys_test = next(dataset_test)
 
     # Compute log-likelihood and accuracy for training and test datasets
     print('Normalized Likelihoods and Accuracies for disRNN')
     print('Training Dataset')
-    compute_log_likelihood_and_accuracy(dataset_train, make_disrnn, disrnn_params)
+    compute_log_likelihood_and_accuracy(xs_train, ys_train, make_disrnn, disrnn_params)
     print('Held-Out Dataset')
-    compute_log_likelihood_and_accuracy(dataset_test, make_disrnn, disrnn_params)
+    compute_log_likelihood_and_accuracy(xs_test, ys_test, make_disrnn, disrnn_params)
+    shuffled_dataset_test = copy.deepcopy(dataset_test)
+    shuffled_dataset_test._ys = np.random.permutation(shuffled_dataset_test._ys)
+    shuffled_xs_test, shuffled_ys_test = next(shuffled_dataset_test)
+    print(shuffled_ys_test.shape)
+    print('Held-Out Shuffled Dataset')
+    compute_log_likelihood_and_accuracy(shuffled_xs_test, shuffled_ys_test, make_disrnn, disrnn_params)
 
-    # Get the first batch of data from the dataset
-    xs_train, act_train = next(dataset_train)
-    xs_test, act_test = next(dataset_test)
 
-    # Evaluate on the first batch
-    pred_train, act_train, xs_train = evaluate(xs_train, act_train, make_disrnn, disrnn_params)
-    pred_test, act_test, xs_test = evaluate(xs_test, act_test, make_disrnn, disrnn_params)
-    df_train = create_dataframe_for_plotting(pred_train, act_train, bitResponseAIsCorr_train[:, :, 0].ravel(), P_A_train[:, :, 0].ravel(), xs_train)
-    df_test = create_dataframe_for_plotting(pred_test, act_test, bitResponseAIsCorr_test[:, :, 0].ravel(), P_A_test[:, :, 0].ravel(), xs_test)
+    pred_train, ys_train, xs_train = evaluate(xs_train, ys_train, make_disrnn, disrnn_params)
+    pred_test, ys_test, xs_test = evaluate(xs_test, ys_test, make_disrnn, disrnn_params)
+    df_train = create_dataframe_for_plotting(pred_train, ys_train, bitResponseAIsCorr_train[:, :, 0].ravel(), P_A_train[:, :, 0].ravel(), xs_train)
+    log_probs_train = np.log(df_train['human_chosen_action_prob'])
+    normalized_likelihood_upper_bound_train = np.exp(np.mean(log_probs_train))
+    print("Model Normalized likelihood Upperbound for training", normalized_likelihood_upper_bound_train)
+    df_test = create_dataframe_for_plotting(pred_test, ys_test, bitResponseAIsCorr_test[:, :, 0].ravel(), P_A_test[:, :, 0].ravel(), xs_test)
+    log_probs_test = np.log(df_test['human_chosen_action_prob'])
+    normalized_likelihood_upper_bound_test = np.exp(np.mean(log_probs_test))
+    print("Model Normalized likelihood Upperbound for testing", normalized_likelihood_upper_bound_test)
 
     # Plot results
+    save_folder_name = saved_checkpoint_pth.split('checkpoints/')[1].split('.pkl')[0]
     plot_training_results(df_train, dataset_train)
     print(df_train.tail(10))
 
-    plot_bottlenecks(disrnn_params)
+    plot_bottlenecks(disrnn_params, save_folder_name)
     session_i = 0
     plot_latent_activations_for_session(sess_i = session_i,
                                         xs_train=xs_train, 
                                         pred_train=pred_train, 
-                                        act_train=act_train, 
+                                        act_train=ys_test, 
                                         bitResponseAIsCorr_train=bitResponseAIsCorr_train, 
                                         P_A_train=P_A_train, 
                                         disrnn_params=disrnn_params, 
@@ -570,22 +567,11 @@ def main():
     # plot_update_rules(disrnn_params, make_disrnn)
 
 
-
-
-
-    '''
-    session_xs_train = xs_train[:, sess_i, :]
-    session_act_train = act_train[:, sess_i, :]
-    sess_i = 0
-    trial_end = rnn_utils.find_session_end(xs_train[:, sess_i, 0])
-    network_outputs, network_states = rnn_utils.eval_model(make_disrnn, disrnn_params, xs_train[:trial_end, sess_i:sess_i+1])
-    network_states = np.array(network_states)
-    disrnn_activations = network_states[:trial_end, sess_i, :]
-    labels = [f'Latent_{i+1}' for i in range(disrnn_activations.shape[1])]
-    choices = df_train["act"]
-    rewards = df_train["rwd_manual"]
-    plot_session(choices, rewards, timeseries=disrnn_activations, timeseries_name='Network Activations', labels=labels)
-    '''
-
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", nargs=1, type=int, default=42, help="Seed for reproducibility.")
+    parser.add_argument("--saved_checkpoint_pth", type=str, required=True, help="Saved checkpoint path for evaluation.")
+
+    args = parser.parse_args()
+
+    main(args.seed, args.saved_checkpoint_pth)
