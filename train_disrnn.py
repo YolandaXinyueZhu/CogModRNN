9342730c4313143859ecb8cee460ef0ba90ced4c
rsw home pc <rw2967@columbia.edu>
2024-10-28 12:37:34 -0400

    added important save checkpoint.

--- train_disrnn.py
+++ train_disrnn.py
@@ -211,8 +211,8 @@ def train_model(args_dict,
         wandb.log({"loss": loss, "step": step})
 
     checkpoint = {
-        'args_dict': args_dict,
-        'disrnn_params': disrnn_params
+    'args_dict': args_dict,
+    'disrnn_params': disrnn_params
     }
 
     filename = os.path.join(
@@ -220,9 +220,12 @@ def train_model(args_dict,
         f'disrnn_params_ls_{latent_size}_umlp_{"-".join(map(str, update_mlp_shape))}_cmlp_{"-".join(map(str, choice_mlp_shape))}_penalty_{penalty_scale}_beta_{beta_scale}_lr_1e-3.pkl'
     )
 
+    # Save the checkpoint using pickle
+    with open(filename, 'wb') as f:
+        pickle.dump(checkpoint, f)
+
     print(f'Saved disrnn_params to {filename}')
-    
-    return disrnn_params
+
 
 def main(args_dict,
          seed,
@@ -265,7 +268,7 @@ if __name__ == "__main__":
     parser.add_argument("--seed", nargs=1, type=int, default=42, help="Seed for reproducibility.")
     parser.add_argument("--validation_proportion", nargs=1, type=int, default=0.1, help="The percentage for validation dataset.")
     parser.add_argument("--latent_size", type=int, default=8, help="Number of latent units in the model")
-    parser.add_argument("--update_mlp_shape", nargs="+", type=int, default=[24, 24, 24], help="Number of hidden units in each of the two layers of the update MLP")
+    parser.add_argument("--update_mlp_shape", nargs="+", type=int, default=[20, 20, 20], help="Number of hidden units in each of the two layers of the update MLP")
     parser.add_argument("--choice_mlp_shape", nargs="+", type=int, default=[18, 18, 18], help="Number of hidden units in each of the two layers of the choice MLP")
     parser.add_argument("--beta_scale", type=float, required=True, help="Value for the beta scaling parameter")
     parser.add_argument("--penalty_scale", type=float, required=True, help="Value for the penalty scaling parameter")
